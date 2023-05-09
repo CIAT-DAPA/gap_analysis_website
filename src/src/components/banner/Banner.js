bf579c46670aa7fce6063123f3c9029ffbb53cf3
Sebastian Lopez <sebastian.lopez_ast@uao.edu.co>
2023-05-09 16:27:35 -0500

    working in parallax

--- src/src/components/banner/Banner.js
+++ src/src/components/banner/Banner.js
@@ -1,7 +1,8 @@
 import "./Banner.css";
-import barley from "../../assets/img/barleyCard.png";
-import bean from "../../assets/img/frijolCard.png";
-import whiteBean from "../../assets/img/whiteCard.png";
+import bean from "../../assets/img/beanCard.jpeg";
+import whiteBean from "../../assets/img/beanWhiteCard.jpeg";
+import barley from "../../assets/img/barleyCard.jpeg";
+import banana from "../../assets/img/bananaCard.jpeg";
 import {
   MouseParallaxContainer,
   MouseParallaxChild,
@@ -10,132 +11,245 @@ import { Row } from "react-bootstrap";
 
 function Banner() {
   return (
-    <MouseParallaxContainer
-      className="parallax"
-      resetOnLeave
-    >
-      <Row>
-        <MouseParallaxChild
-          factorX={0.2}
-          factorY={0.2}
-          className="parallaxChild col text-center"
-          style={{
-            left: "4%",
-            bottom: "50%",
-          }}
-        >
-          <img src={bean} alt="" className="imgParallax" />
-        </MouseParallaxChild>
-        <MouseParallaxChild
-          factorX={0.3}
-          factorY={0.3}
-          className="parallaxChild col text-center"
-          style={{
-            left: "",
-            top: "",
-            scale: "1.3",
-            zIndex: "500"
-          }}
-        >
-          <img src={whiteBean} alt="" className="imgParallax" />
-        </MouseParallaxChild>
-        <MouseParallaxChild
-          factorX={0.5}
-          factorY={0.5}
-          className="parallaxChild col text-center blur"
-          style={{
-            left: "",
-            top: "",
-            scale: "1.5",
-            zIndex: "1000"
-          }}
-        >
-          <img src={barley} alt="" className="imgParallax" />
-        </MouseParallaxChild>
-      </Row>
-      <Row>
-        <MouseParallaxChild
-          factorX={0.3}
-          factorY={0.3}
-          className="parallaxChild col text-center"
-          style={{
-            left: "",
-            top: "0",
-            scale: "1.3",
-            zIndex: "500"
-          }}
-        >
-          <img src={barley} alt="" className="imgParallax" />
-        </MouseParallaxChild>
-        <MouseParallaxChild
-          factorX={0}
-          factorY={0}
-          className="parallaxChild textParallax col text-center"
-          style={{
-            left: "",
-          }}
-        >
-          <h1>GAP Analysis</h1>
-          <p>
-            Welcome! Discover our unique crop accessions on our GAP analysis
-            page.
-          </p>
-        </MouseParallaxChild>
+    <>
+      <MouseParallaxContainer
+        className="parallax d-flex flex-column align-items-center"
+        resetOnLeave
+      >
+        <Row className="flex-nowrap gap-5">
+          <MouseParallaxChild
+            factorX={0.2}
+            factorY={0.2}
+            className="parallaxChild col text-center"
+            style={{
+              left: "10px",
+              bottom: "-10px",
+            }}
+          >
+            <figure style={{ "--c": "#000000a6" }}>
+              <img src={banana} alt="Mountains" className="imgParallax" />
+              <figcaption>
+                The Night
+                <br />
+                <p className="fs-5">
+                  Description of Barley fsad af qw fa a da sd as dqfqfqwq dq dw
+                </p>
+              </figcaption>
+            </figure>
+          </MouseParallaxChild>
+          <MouseParallaxChild
+            factorX={0.3}
+            factorY={0.3}
+            className="parallaxChild col text-center"
+            style={{
+              left: "",
+              top: "",
+              scale: "1.2",
+              zIndex: "1000",
+            }}
+          >
+            <figure style={{ "--c": "#000000a6" }}>
+              <img src={barley} alt="Mountains" className="imgParallax" />
+              <figcaption>
+                The Night
+                <br />
+                <p className="fs-5">
+                  Description of Barley fsad af qw fa a da sd as dqfqfqwq dq dw
+                </p>
+              </figcaption>
+            </figure>
+          </MouseParallaxChild>
+          <MouseParallaxChild
+            factorX={0.2}
+            factorY={0.2}
+            className="parallaxChild col text-center"
+            style={{
+              left: "",
+              top: "",
+              scale: "1.1",
+            }}
+          >
+            <figure style={{ "--c": "#ffffffa6" }}>
+              <img src={bean} alt="Mountains" className="imgParallax" />
+              <figcaption>
+                The Night
+                <br />
+                <p className="fs-5">
+                  Description of Barley fsad af qw fa a da sd as dqfqfqwq dq dw
+                </p>
+              </figcaption>
+            </figure>
+          </MouseParallaxChild>
+          <MouseParallaxChild
+            factorX={0.3}
+            factorY={0.3}
+            className="parallaxChild col text-center"
+            style={{
+              left: "",
+              top: "",
+              scale: "1.1",
+            }}
+          >
+            <figure style={{ "--c": "#000000a6" }}>
+              <img src={whiteBean} alt="Mountains" className="imgParallax" />
+              <figcaption>
+                The Night
+                <br />
+                <p className="fs-5">
+                  Description of Barley fsad af qw fa a da sd as dqfqfqwq dq dw
+                </p>
+              </figcaption>
+            </figure>
+          </MouseParallaxChild>
+        </Row>
+        <Row className="flex-nowrap gap-5">
+          <MouseParallaxChild
+            factorX={0.3}
+            factorY={0.3}
+            className="parallaxChild col text-center"
+            style={{
+              left: "",
+              top: "0",
+              zIndex: "1000",
+            }}
+          >
+            <figure style={{ "--c": "#000000a6" }}>
+              <img src={banana} alt="Mountains" className="imgParallax" />
+              <figcaption>
+                The Night
+                <br />
+                <p className="fs-5">
+                  Description of Barley fsad af qw fa a da sd as dqfqfqwq dq dw
+                </p>
+              </figcaption>
+            </figure>
+          </MouseParallaxChild>
+          <MouseParallaxChild
+            factorX={0.2}
+            factorY={0.2}
+            className="parallaxChild textParallax col text-center"
+            style={{
+              left: "",
+            }}
+          >
+            <h1>GAP Analysis</h1>
+            <p>
+              Welcome! Discover our unique crop accessions on our GAP analysis
+              page.
+            </p>
+          </MouseParallaxChild>
 
-        <MouseParallaxChild
-          factorX={0.3}
-          factorY={0.3}
-          className="parallaxChild col text-center"
-          style={{
-            left: "",
-            top: "",
-            scale: "1.3",
-            zIndex: "500"
-          }}
-        >
-          <img src={barley} alt="" className="imgParallax" />
-        </MouseParallaxChild>
-      </Row>
-      <Row>
-        <MouseParallaxChild
-          factorX={0.5}
-          factorY={0.5}
-          className="parallaxChild col text-center"
-          style={{
-            left: "",
-            top: "",
-            scale: "1.5",
-            zIndex: "1000"
-          }}
-        >
-          <img src={barley} alt="" className="imgParallax" />
-        </MouseParallaxChild>
-        <MouseParallaxChild
-          factorX={0.3}
-          factorY={0.3}
-          className="parallaxChild col text-center"
-          style={{
-            left: "",
-            top: "",
-            scale: "1.3",
-            zIndex: "500"
-          }}
-        >
-          <img src={barley} alt="" className="imgParallax" />
-        </MouseParallaxChild>
-        <MouseParallaxChild
-          factorX={0.2}
-          factorY={0.2}
-          className="parallaxChild col text-center"
-          style={{
-            left: "",
-            top: "",
-          }}
-        >
-          <img src={barley} alt="" className="imgParallax" />
-        </MouseParallaxChild>
-      </Row>
-    </MouseParallaxContainer>
+          <MouseParallaxChild
+            factorX={0.3}
+            factorY={0.3}
+            className="parallaxChild col text-center"
+            style={{
+              left: "",
+              top: "",
+              zIndex: "1000",
+            }}
+          >
+            <figure style={{ "--c": "#000000a6" }}>
+              <img src={barley} alt="Mountains" className="imgParallax" />
+              <figcaption>
+                The Night
+                <br />
+                <p className="fs-5">
+                  Description of Barley fsad af qw fa a da sd as dqfqfqwq dq dw
+                </p>
+              </figcaption>
+            </figure>
+          </MouseParallaxChild>
+        </Row>
+        <Row className="flex-nowrap gap-5">
+          <MouseParallaxChild
+            factorX={0.2}
+            factorY={0.2}
+            className="parallaxChild col text-center"
+            style={{
+              left: "",
+              top: "",
+              scale: "1.2",
+            }}
+          >
+            <figure style={{ "--c": "#000000a6" }}>
+              <img src={bean} alt="Mountains" className="imgParallax" />
+              <figcaption>
+                The Night
+                <br />
+                <p className="fs-5">
+                  Description of Barley fsad af qw fa a da sd as dqfqfqwq dq dw
+                </p>
+              </figcaption>
+            </figure>
+          </MouseParallaxChild>
+          <MouseParallaxChild
+            factorX={0.3}
+            factorY={0.3}
+            className="parallaxChild col text-center"
+            style={{
+              left: "",
+              top: "",
+              scale: "1.1",
+              zIndex: "1000",
+            }}
+          >
+            <figure style={{ "--c": "#000000a6" }}>
+              <img src={barley} alt="Mountains" className="imgParallax" />
+              <figcaption>
+                The Night
+                <br />
+                <p className="fs-5">
+                  Description of Barley fsad af qw fa a da sd as dqfqfqwq dq dw
+                </p>
+              </figcaption>
+            </figure>
+          </MouseParallaxChild>
+          <MouseParallaxChild
+            factorX={0.2}
+            factorY={0.2}
+            className="parallaxChild col text-center"
+            style={{
+              left: "",
+              top: "",
+            }}
+          >
+            <figure style={{ "--c": "#000000a6" }}>
+              <img src={whiteBean} alt="Mountains" className="imgParallax" />
+              <figcaption>
+                The Night
+                <br />
+                <p className="fs-5">
+                  Description of Barley fsad af qw fa a da sd as dqfqfqwq dq dw
+                </p>
+              </figcaption>
+            </figure>
+          </MouseParallaxChild>
+          <MouseParallaxChild
+            factorX={0.3}
+            factorY={0.3}
+            className="parallaxChild col text-center"
+            style={{
+              left: "",
+              top: "",
+              scale: "1.2",
+            }}
+          >
+            <figure style={{ "--c": "#000000a6" }}>
+              <img src={banana} alt="Mountains" className="imgParallax" />
+              <figcaption>
+                The Night
+                <br />
+                <p className="fs-5">
+                  Description of Barley fsad af qw fa a da sd as dqfqfqwq dq dw
+                </p>
+              </figcaption>
+            </figure>
+          </MouseParallaxChild>
+        </Row>
+      </MouseParallaxContainer>
+      <div className="divider" />
+    </>
   );
 }
 
